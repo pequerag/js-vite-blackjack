fbf9dbb2672cf0b66cbb77049603b7998436fc1a
Germán <45312492+pequerag@users.noreply.github.com>
2023-01-19 15:15:32 -0300

    first commit

--- src/blackjarck/usescases/index.js
+++
@@ -1,6 +0,0 @@
-//import { crearDeck as crearNuevoDeck } from './usescases/crear-deck';
-export { crearCartaHTML } from './crear-carta-html';
-export { crearDeck } from './crear-deck';
-export { pedirCarta} from './pedir-carta';
-export { turnoComputadora } from './turno-computadora';
-export { valorCarta } from './valor-carta';
